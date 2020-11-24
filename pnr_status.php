8c88fc905962cb2e35104ecdd34338066b3bba40
sarveshvhawal007 <56724225+sarveshvhawal007@users.noreply.github.com>
2020-11-24 22:04:55 +0530

    updated

--- pnr_status.php
+++ pnr_status.php
@@ -2,9 +2,29 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title></title>
+    <title>Check PNR Status</title>
   </head>
   <body>
-    Empty
+    <form class="" action="pnr_status.php" method="post">
+      <div class="">
+        <label for="">Enter PNR Number</label>
+        <input type="text" name="pnr" value="">
+      </div>
+      <button type="submit" name="button">Generate Ticket</button>
+
+    </form>
   </body>
 </html>
+<?php
+if(isset($_POST['button']))
+
+{
+  $pnr=$_POST['pnr'];
+
+  $sql=mysqli_query("select * from ticket_desc_future where PNR_number=='$pnr'");
+  $row=mysqli_fetch_array($sql);
+  echo "<hr>";
+  print_r($row);
+}
+
+ ?>
