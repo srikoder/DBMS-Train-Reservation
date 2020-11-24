faaca787164549f2db93e114f34e95c48efd08df
sarveshvhawal007 <56724225+sarveshvhawal007@users.noreply.github.com>
2020-11-24 22:02:51 +0530

    updated

--- book.php
+++ book.php
@@ -2,15 +2,15 @@
 include('connect.php');
 error_reporting(0);
 session_start();
-//  $passanger=$_POST['passanger_count'];
-  $train_id=$_SESSION['trainid'];
+//  $passenger=$_POST['passenger_count'];
+  $train_id=$_SESSION['train_id'];
   $todaysdate=$_SESSION['datee'];
 
   // run query for ac seats;
-  echo "<br>Train ID: ".$train_id;
+  echo "<hr><br>Train ID: ".$train_id;
   echo "<br>Date of Running: ".$todaysdate;
-  echo "<br>Available AC coachs: ".$row['remain_AC_seats'];
-  echo "<br>Available Sleeper Coachs: ".$row['remain_SL_seats'];
+  echo "<br>Available AC coachs: ".$_SESSION['remain_ac'];
+  echo "<br>Available Sleeper Coachs: ".$_SESSION['remain_sl'];
  ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
@@ -23,9 +23,9 @@ session_start();
   <body>
 <h4>Booking Details</h4>
 <form class="formm" action="book.php" method="post">
-  <div class="passanger_no">
-    <label for="">Total Passangers:-</label>
-    <input type="text" name="passanger_count" value="">
+  <div class="passenger_no">
+    <label for="">Total passengers:-</label>
+    <input type="text" name="passenger_count" value="">
   </div>
   <div>
     <label for="">Seats Type:-</label>
@@ -41,43 +41,55 @@ session_start();
   </body>
 </html>
 <?php
-  $passanger=$_POST['passanger_count'];
-  $_SESSION['passanger']=$passanger;
+if(isset($_POST['button'])){
+  $passenger=$_POST['passenger_count'];
+  $_SESSION['passenger']=$passenger;
   $seat=$_POST['seat'];
+  $berth_number="";
+  $coach_number="";
   $_SESSION['seat']=$seat;
-
-  // check for passanger_count
+  $check=1;
+  // check for passenger_count
   if($seat=='AC')
   {
-    if($_SESSION['remain_ac']<$passanger)
+    if($_SESSION['remain_ac']<$passenger)
     {
-      echo "Kill ".$_SESSION['remain_ac']-$passanger." Passangers";
+      echo "Error  <hr>".$passenger." Seats not Available";
+      $check=0;
+
     }
     else {
       $coach_no=$_SESSION['remain_ac'];
-      $_SESSION['select']='1';
       $_SESSION['coach_no']=$coach_no;
+      $coach_number=$coach_no/18+1;
+      $berth_number=$coach_no%18+1;
 
     }
   }
   if($seat=='SL')
   {
-    if($_SESSION['remain_sl']<$passanger)
+    if($_SESSION['remain_sl']<$passenger)
     {
-      echo "Kill ".$_SESSION['remain_sl']-$passanger." Passangers";
+      echo "Error  <hr>".$passenger." Seats not Available";
+      $check=0;
     }
     else {
       $coach_no=$_SESSION['remain_sl'];
-      $_SESSION['select']='2';
       $_SESSION['coach_no']=$coach_no;
+      $coach_number=$coach_no/24+1;
+      $berth_number=$coach_no%24+1;
 
     }
   }
   // generate PNR Number
   // insert Ticket into ticket mysql
-  $pnr=$train_id.$name.$todaysdate.$coach_no;
+  if($check){
+  $pnr=$train_id.$todaysdate.$coach_number.$berth_number;
+  $_SESSION['coach_number']=$coach_number;
+  $_SESSION['berth_number']=$berth_number;
+
   $_SESSION['pnr']=$pnr;
-  $x=$passanger;
+  $x=$passenger;
   if($x>0){
   echo "Generated PNR no:-".$pnr."<br>";
   echo "<form action='booking_complete.php' method='post'>";
@@ -87,13 +99,15 @@ session_start();
     echo "<label> Age:-</label><input type='text' name='age$x' value>";
     echo "<label> Gender:-</label>
     <input type='radio' name='gen$x' value='M'> Male
-    <input type='radio' name='gen$x' value='F'>Female
+    <input type='radio' name='gen$x' value='F'> Female
     ";
     $x--;
     echo"<br>";
   }
 echo "<button type='submit' class='button button1' name='button'>Complete Registration</button>";
   echo "</form>";
+}
+}
 }
     // insert data here
  ?>
