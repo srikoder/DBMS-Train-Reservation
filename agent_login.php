ddc513e6b4e168aa76120c9b09654ff40834c77a
sarveshvhawal007 <56724225+sarveshvhawal007@users.noreply.github.com>
2020-11-26 22:28:51 +0530

    updated

--- agent_login.php
+++ agent_login.php
@@ -5,144 +5,164 @@ error_reporting(0);
 
  ?>
 <!DOCTYPE html>
-<html>
-  <head>
-    <script language="javascript" type="text/javascript">
-      window.history.forward();
-    </script>
-    <title>LOGIN</title>
-    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700" rel="stylesheet">
+<html lang="en" dir="ltr">
+
+<head>
+    <meta charset="utf-8">
+    <title>Take Attendence</title>
+    <link rel="stylesheet" href="">
+    <!-- <script type="text/javascript" src="script.js"></script> -->
     <style>
-      html, body {
-      background-color: #f5f5f5;
-      display: flex;
-      justify-content: center;
-      font-family: montserrat;
-      font-size: 15px;
-      }
-      form {
-      text-align: center;
-      margin: 50px;
-      width: 800px;
-      height: 500px;
-      background-color: #efe;
-      border-radius: 20px;
-      border: 5px solid #81fa63;
-     box-shadow: 5px 10px 8px #dbd5d5;
-      }
-      input[type=text], input[type=password] {
-      width: 70%;
-      padding: 16px 8px;
-      margin: 5px 15% ;
-      display: block;
-      border: none;
-      border-bottom: 4px solid #8ebf42;
-      font-size: 18px;
-      /* text-align: center; */
-       /* border: px solid #6cff69;  */
-      box-sizing: border-box;
+        * {
+            font-family: 'Baloo Bhai 2', cursive;
+        }
 
-      }
-      button {
-      display: block;
-      align-items: center;
-      background-color: #8ebf42;
-      color: white;
-      padding: 10px;
-      margin: 25px 25%;
-      border: none;
-      border-radius: 10px;
-      outline: none;
-      background-size: 200%;
-      cursor: pointer;
-      text-align: center;
-      width: 50%;
-    }
-      h1 {
-        /* text-shadow: 2px 2px 15px #80ff90; */
-      color: #51cc61;
-      text-align:center;
-      font-size: 60px;
-      padding-top: 2px;
-      }
-      br{
-      	padding: 15px;
-      }
-      hr{
-        border: 2px solid #51cc61;
-      }
-      h5{
-        color: red;
-      }
-      button:hover {
-      opacity: 0.8;
+        body {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            background-image: url("https://cdn.pixabay.com/photo/2016/03/05/23/02/blur-1239439_1280.jpg");
+            /* background-repeat: repeat-y; */
+            background-repeat: no-repeat;
+            background-size: cover;
+        }
 
-      }
-      input:focus {
-    background-color: lightblue;
-  border: 1px solid #51cc61;
-  }
-     img
-      {
-        padding: 5px 5px;
-        margin: 1%;
-        display: inline-block;
-        width: 150px;
-        height: 150px;
+        .imp {
+            font-size: large;
+            color: black;
+            background-color: #cbcac8a8;
+            text-align: center;
+            width: 45%;
+            display: block;
+            margin-top: 108px;
+            padding: 20px;
+            border: 2px solid black;
+            border-radius: 15px;
+        }
 
+        h1 {
+            margin: 0px;
+            padding: 0px;
+            color: brown;
+            text-align: center;
+        }
 
-      }
-      .formcontainer {
+        input {
+            border: 2px solid black;
+            font-size: 24px;
+            text-align: center;
+            height: 31px;
+            border-radius: 10px;
+            width: 100%;
+            margin: 10px 0px;
+            display: inline;
+        }
 
-      text-align: left;
-      padding: 5px 5px;
-      margin: 14px 50px 12px;
-      }
+        input[type="radio"] {
+            height: 2em;
+            width: 3%;
+            display: inline;
+            margin: 0px;
+            /* border: 2px solid black; */
+        }
 
-      span.pssw {
-        display: block;
-      text-align:center;
-      font-size: 15px;
-      float: center;
-      padding: 5px 0px;
-      }
+        button {
+            border: 3px solid black;
+            margin: auto;
+            font-size: 24px;
+            text-align: center;
+            height: 40px;
+            border-radius: 10px;
+            width: 60%;
+            margin-top: 10px;
+            display: block;
+        }
 
-      /* Change styles for span on extra small screens */
-      @media screen and (max-width: 300px) {
-      }
-      span.psw {
-      display: block;
-      float: none;
-      }
+        label {
+            display: block;
+            margin-top: 20px;
+            font-size: 31px;
+        }
+
+        form {
+            margin-top: 20px;
+        }
+
+        a {
+            position: relative;
+            top: 10px;
+            text-decoration: none;
+            /* background-color: rgb(155, 155, 155, 0.6); */
+            color: rgb(0, 0, 0);
+        }
+
+        button:hover {
+            height: 50px;
+            border-radius: 10px;
+            width: 70%;
+            cursor: pointer;
+            background-color: black;
+            color: rgb(255, 255, 255);
+        }
+
+        a:hover {
+            text-decoration: none;
+            background-color: black;
+            color: rgb(255, 255, 255);
+            border-radius: 5px;
+            padding: 7px;
+            margin-top: 10px;
+        }
+
+        p {
+            display: inline;
+            margin: 0px;
+            color: rgb(255, 0, 0);
+        }
+
+        .ac {
+            display: inline;
+            /* margin-right: 121px; */
+        }
+
+        .sleeper {
+            display: inline;
+            /* margin-right: 53px; */
+        }
+
+        .trouble {
+            margin-bottom: 20px;
+        }
     </style>
-  </head>
-  <body>
+</head>
 
-    <form action="agent_login.php" method="post">
-    <!-- <div>
-     <img src="logo.png">
-  </div> -->
+<body>
+    <div class="imp">
+        <form action="agent_login.php" method="post">
+            <!-- <div>
+         <img src="logo.png">
+      </div> -->
 
 
-      <h1><b>Agent Login</b></h1>
-      <div class="formcontainer">
-      <hr>
-      <div class="container">
+            <h1><b>Agent Login</b></h1>
+            <div class="formcontainer">
+                <hr>
+                <div class="container">
 
-        <input type="text" placeholder="Agent ID" name="user" required>
-        <br>
-        <input type="password" placeholder="Password" name="psw" required>
-      </div>
-      <button type="submit" name="submit" ><h3>Login</h3></button>
-      <span class="pssw"><a href="#"> Forgot password?</a></span>
-      <span class="pssw"><a href="index.php"> Go Back?</a></span>
+                    <input type="text" placeholder="Agent ID" name="user" required>
+                    <br>
+                    <input type="password" placeholder="Password" name="psw" required>
+                </div>
+                <button type="submit" name="submit">Login</button>
+                <span class="pssw"><a href="#"> Forgot password?</a></span>
+                <span class="pssw"><a href="index.php"> Go Back?</a></span>
 
-    </form>
-  </body>
+        </form>
+        </div>
+
+</body>
+
 </html>
-
-
 
 <?php
 if(isset($_POST['submit'])){
